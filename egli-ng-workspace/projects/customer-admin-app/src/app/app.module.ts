2af27de5efc553c770473978f5172f8f0536137e
MichaelEgli <michaelegli1879@gmail.com>
2022-03-28 20:34:36 +0200

    add component customers

--- egli-ng-workspace/projects/customer-admin-app/src/app/app.module.ts
+++ egli-ng-workspace/projects/customer-admin-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/layout/header/header.component';
+import { CustomersComponent } from './projects/features/customers/customers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule,
