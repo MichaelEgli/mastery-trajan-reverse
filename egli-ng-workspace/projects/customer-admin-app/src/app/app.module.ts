38c76e52709437b4cc7bfcafb79718f3fd271ae4
MichaelEgli <michaelegli1879@gmail.com>
2022-03-28 21:07:27 +0200

    add routing to customers component

--- egli-ng-workspace/projects/customer-admin-app/src/app/app.module.ts
+++ egli-ng-workspace/projects/customer-admin-app/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/layout/header/header.component';
-import { CustomersComponent } from './projects/features/customers/customers.component';
+import { CustomersComponent } from './features/customers/customers.component';
 
 @NgModule({
   declarations: [
