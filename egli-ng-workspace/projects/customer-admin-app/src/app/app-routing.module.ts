91f0a2bcb53076df48b913756ae3f4d4aacd96ae
MichaelEgli <michaelegli1879@gmail.com>
2022-03-28 21:28:06 +0200

    add routing home

--- egli-ng-workspace/projects/customer-admin-app/src/app/app-routing.module.ts
+++ egli-ng-workspace/projects/customer-admin-app/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomersComponent } from './features/customers/customers.component';
+import { HomeComponent } from './features/home/home.component';
 
 const routes: Routes = [
   {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
     path: 'customers', component: CustomersComponent
-  }
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
 ];
 
 @NgModule({
